fd10d36760b838198de4ce06be63444aed07b78f
rcooke-ast <rcooke.ast@gmail.com>
2020-04-23 10:25:49 +0100

    multiple updates

--- examples/prepfit/select_fitting_regions.py
+++ examples/prepfit/select_fitting_regions.py
@@ -8,24 +8,24 @@ matplotlib.use('Qt5Agg')
 from alis.prepfit import specplot
 
 if __name__ == '__main__':
-	import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
 
-	dla = dlas("object1")
-	prop = specplot.props(dla)
+    dla = dlas("object1")
+    prop = specplot.props(dla)
 
-	# Ignore lines outside of wavelength range
-	wmin, wmax = np.min(prop._wave)/(1.0+prop._zabs), np.max(prop._wave)/(1.0+prop._zabs)
+    # Ignore lines outside of wavelength range
+    wmin, wmax = np.min(prop._wave)/(1.0+prop._zabs), np.max(prop._wave)/(1.0+prop._zabs)
 
-	# Load atomic data
-	atom = specplot.atomic(wmin=wmin, wmax=wmax)
+    # Load atomic data
+    atom = specplot.atomic(wmin=wmin, wmax=wmax)
 
-	spec = Line2D(prop._wave, prop._flux, linewidth=1, linestyle='solid', color='k', drawstyle='steps', animated=True)
+    spec = Line2D(prop._wave, prop._flux, linewidth=1, linestyle='solid', color='k', drawstyle='steps', animated=True)
 
-	fig, ax = plt.subplots(figsize=(16,9), facecolor="white")
-	ax.add_line(spec)
-	reg = specplot.SelectRegions(fig.canvas, ax, spec, prop, atom)
+    fig, ax = plt.subplots(figsize=(16,9), facecolor="white")
+    ax.add_line(spec)
+    reg = specplot.SelectRegions(fig.canvas, ax, spec, prop, atom)
 
-	ax.set_title("Press '?' to list the available options")
-	#ax.set_xlim((prop._wave.min(), prop._wave.max()))
-	ax.set_ylim((0.0, prop._flux.max()))
-	plt.show()
+    ax.set_title("Press '?' to list the available options")
+    #ax.set_xlim((prop._wave.min(), prop._wave.max()))
+    ax.set_ylim((0.0, prop._flux.max()))
+    plt.show()
