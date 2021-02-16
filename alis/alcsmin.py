585c370c275f9074d34585ab0170a79915668aa6
rcooke-ast <rcooke.ast@gmail.com>
2021-02-16 18:09:13 +0000

    param fix

--- alis/alcsmin.py
+++ alis/alcsmin.py
@@ -1831,7 +1831,7 @@ class alfit(object):
 #						else:
                         #mout = self.alisdict._funcarray[1][mtyp].call_CPU(self.alisdict._funcarray[2][mtyp], wave, pararr[sp][sn][ea][md], ae=aetag, mkey=keyarr[sp][sn][ea][md])
                         for mm in range(0, pararr[sp][sn][ea][md].shape[0]):
-                            self.gpu_prepare(mtyp, parstr, gpustr, pin,
+                            self.gpu_prepare(mtyp, parstr, gpustr, pararr[sp][sn][ea][md][mm,:],
                                              ae=aetag, mkey=[keyarr[sp][sn][ea][md][mm]],
                                              shift_vel=shift_vel, shift_ang=shift_ang,
                                              cont=keyarr[sp][sn][ea][md][mm]['continuum'], ncpus=1)
